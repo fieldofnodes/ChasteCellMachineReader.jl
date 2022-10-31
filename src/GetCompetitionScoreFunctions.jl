25664045db62586598e43c10eac8e34d818fad80
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2022-10-31 01:14:52 +0000

    Added new comp functions

--- src/GetCompetitionScoreFunctions.jl
+++ src/GetCompetitionScoreFunctions.jl
@@ -42,13 +42,13 @@ end
 
     The function returns the time shifted time series, the time series for cell label `0` and `1` and the time series competition.
 """
-function get_population_competition_time_series(::MachineState,cell_data)
+function get_population_competition_time_series(::MachineState,cell_data,growth_rate₁,growth_rate₂)
     time_vars = get_time_vec_variants(MachineState(),cell_data)
 
     population_counts_per_time_step = [get_population_counts_per_time_step(cell_data,t) for t in time_vars.unique_time_vec]
     time_series₁ = [p.count_label_0 for p in population_counts_per_time_step]
     time_series₂ = [p.count_label_1 for p in population_counts_per_time_step]
-    competition = get_competition_score_function(time_series₁,time_series₂,time_vars.unique_shifted_time_vec)
+    competition = get_competition_score_function(MachineState(),time_series₁,time_series₂,time_vars.unique_shifted_time_vec,growth_rate₁,growth_rate₂)
 
 
 
