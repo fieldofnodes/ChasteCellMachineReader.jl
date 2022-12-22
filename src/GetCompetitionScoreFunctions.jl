be5b91841da7ee2db599a2fb4e679641b67f4d60
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2022-12-22 18:39:10 +0000

    Update GetCompetitionScoreFunctions.jl

--- src/GetCompetitionScoreFunctions.jl
+++ src/GetCompetitionScoreFunctions.jl
@@ -48,9 +48,11 @@ function get_population_competition_time_series(::MachineState,cell_data,growth_
     population_counts_per_time_step = [get_population_counts_per_time_step(cell_data,t) for t in time_vars.unique_time_vec]
     time_series₁ = [p.count_label_0 for p in population_counts_per_time_step]
     time_series₂ = [p.count_label_1 for p in population_counts_per_time_step]
-    competition = get_competition_score_function(MachineState(),time_series₁,time_series₂,time_vars.unique_shifted_time_vec,growth_rate₁,growth_rate₂)
+   # competition = 
+   #     get_competition_score_function(
+   #         MachineState(),time_series₁,time_series₂,time_vars.unique_shifted_time_vec,growth_rate₁,growth_rate₂)
 
 
 
-    return MachineStateTimeSeries(time_vars.unique_shifted_time_vec,time_series₁,time_series₂,competition)
+    return MachineStateTimeSeries(time_vars.unique_shifted_time_vec,time_series₁,time_series₂)
 end
