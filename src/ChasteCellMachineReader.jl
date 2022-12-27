f32783276fd2ae98f38151733abfe83b02385bb9
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2022-12-27 23:00:06 +0000

    Added the functions

--- src/ChasteCellMachineReader.jl
+++ src/ChasteCellMachineReader.jl
@@ -24,7 +24,36 @@ export
     get_type_count_per_time,
     get_population_counts_per_time_step,
     get_competition_score_function,
-    get_population_competition_time_series
+    get_population_competition_time_series,
+    cₐ,
+    cₗ,
+    today_nog_gap,
+    N₀,
+    R₀,
+    dᵣ,
+    β,
+    get_norm_vec,
+    shift_scale_uniform,
+    get_mean_cycle_time,
+    get_time_series_labels_diff_to_cell,
+    get_cell_population_ts,
+    get_count_neighbour_targets,
+    get_mean_neighbours_at_time_time_slices,
+    get_mean_competition,
+    compute_target_death_rate,
+    get_competition_per_params,
+    get_mean_comp,
+    plot_population_neighbours_competition,
+    convert_mat_to_df,
+    compute_lm_predict,
+    fit_line,
+    compute_pop_ode_sol,
+    compute_time_extinction,
+    simulation_population_TS,
+    simulation_analytical,
+    get_neighbour_radius,
+    get_radius,
+    get_realisation_number
 
 
 include("StructsAndTypes.jl")
@@ -32,6 +61,7 @@ include("GetCellDataFunctions.jl")
 include("GetDataPerTimeFunctions.jl")
 include("GetTimeRelevantFunctions.jl")
 include("GetCompetitionScoreFunctions.jl")
+include("HelperFunctions.jl")
 end
 
 
--- src/HelperFunctions.jl
+++ src/HelperFunctions.jl
@@ -12,7 +12,17 @@ cₐ(r,l) = π*r^2 + 2*r*l
 cₗ(r,l)  = 2*r + l
 
 
+"""
+    Using Dates package, get today's date and convert to YYYMMDD format
+"""
+today_nog_gap() = replace(today() |> string,"-" => "")
 
+
+
+"""
+    Compute the N₀ given a Float64 values, in this case
+    the Float64 is the time to death
+"""
 function N₀(tₐ)
     r = 0.5
     l = 3.0
@@ -23,6 +33,36 @@ function N₀(tₐ)
     return (2d/l*gᵣ)^2 *(pi*a)
 end
 
+
+"""
+    Compute the N₀ given the death rate
+"""
+function N₀(d::DeathRate)
+    r = 0.5
+    l = 3.0
+    l = cₗ(r,l)
+    a = cₐ(r,l)
+    gᵣ = log(2)/1.15
+    return (2*d.d/(l*gᵣ))^2 *(π*a)
+end
+
+"""
+    Compute the N₀ given the time to death
+"""
+function N₀(d::DeathRateTime)
+    r = 0.5
+    l = 3.0
+    l = cₗ(r,l)
+    a = cₐ(r,l)
+    d = log(2)/d.d
+    gᵣ = log(2)/1.15
+    return (2d/l*gᵣ)^2 *(π*a)
+end
+
+"""
+    Compute the N₀ given the apoptosis (or any other time to death)
+    time
+"""
 function N₀(tₐ::ApoptosisTime)
     r = 0.5
     l = 3.0
@@ -33,6 +73,9 @@ function N₀(tₐ::ApoptosisTime)
     return (2d/l*gᵣ)^2 *(pi*a)
 end
 
+"""
+    Compute the N₀ given the R₀
+"""
 function N₀(R₀::Radius)
     r = 0.5
     l = 3.0
@@ -41,6 +84,9 @@ function N₀(R₀::Radius)
     return π*R₀.r^2/a
 end
 
+"""
+    Compute the radius given the population size
+"""
 function R₀(N₀)
     r = 0.5
     l = 3.0
@@ -48,40 +94,59 @@ function R₀(N₀)
     return sqrt(N₀*a/π)
 end
 
+"""
+    Compute the death rate given the N₀, ther growth rate and the 
+    length and area of the cell.
+"""
+function dᵣ(N₀)
+    r = 0.5
+    l = 3.0
+    l = cₗ(r,l)
+    a = cₐ(r,l)
+    gᵣ = log(2)/1.15
+    return ((l*gᵣ)*√(N₀/(π*a)))/2
+end
 
 
+"""
+    Compute the death rate from the time to death type
+"""
+function dᵣ(t::TimeForDeathRate)
+    return DeathRate(log(2)/t.t)
+end
+    
+"""
+    Compute β from the death rate
+"""
+function β(d::DeathRate)
+    cᵣ = 0.5
+    cylₗ = 3.0
+    l = cₗ(cᵣ,cylₗ)
+    a = cₐ(cᵣ,cylₗ)
+    return (2*d.d*√(π*a))/(l)
+end
+
+
+"""
+    Normalise a vector, x such that x∈[0,1]
+"""
 function get_norm_vec(x) 
     return (x .- minimum(x)) ./ (maximum(x) .- minimum(x))
 end
 
+"""
+    Shift a U(0,1) to U(a,b)
+"""
 function shift_scale_uniform(min_value,max_value)
     range_dif = abs(min_value - max_value)
     return range_dif*rand()+min_value
 end
 
-
+"""
+    Get mean values from a U(a,b)
+"""
 function get_mean_cycle_time(cycle_min_time,cycle_max_time,runs)
     return mean([shift_scale_uniform(cycle_min_time,cycle_max_time) for i in 1:runs])
-end
-
-
-
-
-
-function get_competition_per_params(path,growth_rate₁,growth_rate₂)
-    mat_data = readdlm(path)
-    cell_data = get_cell_data(MachineState(),mat_data)
-    time_series = 
-        get_population_competition_time_series(
-            MachineState(),
-            cell_data,
-            growth_rate₁,growth_rate₂)
-    return time_series.competition[end]
-end
-
-
-function get_mean_comp(competition_score)
-    return sum(competition_score)/length(competition_score)
 end
 
 """
@@ -101,10 +166,13 @@ function get_time_series_labels_diff_to_cell(::MachineState,cell_data)
 end
 
 
-today_nog_gap() = replace(today() |> string,"-" => "")
 
 
 
+"""
+    Get cell population time series, as well as 
+    get time series of neighbours different to cell type.
+"""
 function get_cell_population_ts(data,growth_rate₁,growth_rate₂)
     @chain data begin
         readdlm(_)
@@ -124,61 +192,25 @@ end
 
 
 
-function get_neighbour_radius(data_path)
-    neighbour_radius_str = @chain data_path begin
-        dirname(_)
-        dirname(_)
-        dirname(_)
-        dirname(_)
-        basename(_)
-    end
-    return neighbour_radius_str
-end
-
-function get_radius(data_path) 
-    radius_str = @chain data_path begin
-        dirname(_)
-        dirname(_)
-        dirname(_)
-        basename(_)
-        replace(_,"Radius_" => "")
-        parse(Float64,_)
-    end
-    return radius_str
-end
-
-function get_realisation_number(data_path)
-    realisation = @chain data_path begin
-        dirname(_)
-        dirname(_)
-        basename(_)
-        replace(_,"Realisation_" => "")
-        parse(Float64,_)
-    end
-    return realisation
-end
 
 
-function get_mean_competition(
-    data_path,filter_pattern,
-    growth_rate₁,growth_rate₂)
-    
-    c̄ =@chain data_path begin
-        filter(x -> occursin(filter_pattern,x),_)
-        [get_cell_population_ts.(i,growth_rate₁,growth_rate₂).MachineStateTimeSeries.competition[end] for i in _]
-        mean(_)
-    end
-
-    return c̄
-end
-
+"""
+    Compute the count of neighbours to targets at a vector of time slice indices.
+"""
 function get_count_neighbour_targets(data::CellTimeSeries,time_slices)
-    p⃗₀ = data.MachineStateTimeSeries.label₀    
-    np⃗₀ = data.CellLabelsTimeSeries.neighbour_to_label_0 ./ p⃗₀
+    p⃗₀ = data.MachineStateTimeSeries.target    
+    np⃗₀ = data.CellLabelsTimeSeries.neighbour_to_target ./ p⃗₀
     return np⃗₀[time_slices]
 end
 
 
+"""
+    Take a data pather, filter to appropriate data paths,
+    load data and get time series.
+    Use `get_count_neighbour_targets` functions to get count of neighbours 
+    to targets at time indices.
+    Get mean values
+"""
 function get_mean_neighbours_at_time_time_slices(
     data_path,filter_pattern,
     growth_rate₁,growth_rate₂,time_slices)
@@ -193,32 +225,36 @@ function get_mean_neighbours_at_time_time_slices(
     return mean_neighbours
 end
     
-function convert_mat_to_df(mat)
-    sz_mat = size(mat,2)
-    df = @chain mat begin
-        DataFrame(_,:auto) 
-        rename(_,:x1 => :radius)
-        rename(_,:x2 => :comp)
-        rename(_,["x$i" => "x$(i-2)" for i in 3:sz_mat])
+
+
+
+
+"""
+    Takes data path to a folder of realisations.
+    Filters data paths according to a filter_pattern.
+    Computes time series for cell Population.
+    Computed the so-called competition of the attackers and 
+    takes the mean of the last element per time series.
+    This assumes there is convergence.
+"""
+function get_mean_competition(
+    data_path,filter_pattern,
+    growth_rate₁,growth_rate₂)
+    
+    c̄ =@chain data_path begin
+        filter(x -> occursin(filter_pattern,x),_)
+        [get_cell_population_ts.(i,growth_rate₁,growth_rate₂).MachineStateTimeSeries.competition[end] for i in _]
+        mean(_)
     end
-    return df
+
+    return c̄
 end
 
-function compute_lm_predict(df)
-    d̂f = []
-    R² = []
-    for x ∈ names(df[:,Not([:radius,:comp])])
-        model_formula = term(:comp) ~ term((x))
-        model = lm(model_formula,df)         
-        ŷ = predict(model, df)
-        push!(R²,r2(model))
-        push!(d̂f,ŷ)
-    end
-    predict_df = DataFrame(d̂f,:auto)
 
-    return (predict_df = predict_df,R² = R²)
-end
-
+"""
+    Compute per capita death rate for targets on the circular
+    boundary dividing the targets and attackers.
+"""
 function compute_target_death_rate(t,N,cell_cycle_time,l,a)
     r = log(2) ./ cell_cycle_time
     prod₁ = (l .* r) ./ (2 .* sqrt.(π .* a))
@@ -228,8 +264,36 @@ function compute_target_death_rate(t,N,cell_cycle_time,l,a)
 end
 
 
+"""
+    Load data path.
+    Get cell data.
+    Get the population time series and 
+    competition time series.
+"""
+function get_competition_per_params(path,growth_rate₁,growth_rate₂)
+    mat_data = readdlm(path)
+    cell_data = get_cell_data(MachineState(),mat_data)
+    time_series = 
+        get_population_competition_time_series(
+            MachineState(),
+            cell_data,
+            growth_rate₁,growth_rate₂)
+    return time_series.competition[end]
+end
+
+"""
+    Take a competition score vector.
+    Compute the sum divided by the length of the vector.
+    Return the arithmetic mean of the compeition score.
+"""
+function get_mean_comp(competition_score)
+    return sum(competition_score)/length(competition_score)
+end
 
 
+"""
+    Take data::CellTimeSeries and plot competition per neighbours
+"""
 function plot_population_neighbours_competition(data::CellTimeSeries,radius)
 
     t⃗ = data.MachineStateTimeSeries.time       
@@ -260,6 +324,51 @@ function plot_population_neighbours_competition(data::CellTimeSeries,radius)
     return f
 end
 
+
+"""
+    Convert a matrix to a dataframe.
+    Use the `:auto` variable.
+    `:x1` is the radius of the target boundary
+    `:x2`` is the competition value
+    Then the variables from `:x3` onwards is shifted
+    to `:x1` and so on.
+"""
+function convert_mat_to_df(mat)
+    sz_mat = size(mat,2)
+    df = @chain mat begin
+        DataFrame(_,:auto) 
+        rename(_,:x1 => :radius)
+        rename(_,:x2 => :comp)
+        rename(_,["x$i" => "x$(i-2)" for i in 3:sz_mat])
+    end
+    return df
+end
+
+
+"""
+    Take a dataframe generated from `convert_mat_to_df()`.
+    Generate a line of best fit using the lm model from the GLM 
+    package.
+"""
+function compute_lm_predict(df)
+    d̂f = []
+    R² = []
+    for x ∈ names(df[:,Not([:radius,:comp])])
+        model_formula = term(:comp) ~ term((x))
+        model = lm(model_formula,df)         
+        ŷ = predict(model, df)
+        push!(R²,r2(model))
+        push!(d̂f,ŷ)
+    end
+    predict_df = DataFrame(d̂f,:auto)
+
+    return (predict_df = predict_df,R² = R²)
+end
+
+"""
+    Take two vectors, `x` and `y` and compute the line of best fit
+    manually. Return the line of `ŷ`.
+"""
 function fit_line(x,y)
     ybar = mean(y)
     xbar = mean(x)
@@ -272,87 +381,20 @@ function fit_line(x,y)
 end 
 
 
-
+"""
+    Compute the N(t) ode of the per capita targets as they sit on 
+    the boundary of the circle between the targets and attackers.
+"""
 function compute_pop_ode_sol(t,N₀,r,b) 
-    br = (b ./ r)
+    βr = (β ./ r)
     rt2 = (r.*t) ./ 2 
-    pop = (br .+ (√N₀ .- br) .* exp.(rt2)) .^2
+    pop = (βr .+ (√N₀ .- βr) .* exp.(rt2)) .^2
     return pop 
 end
 
-cₐ(r,l) = π*r^2 + 2*r*l
-cₗ(r,l)  = 2*r + l
-
-function N₀(d::DeathRate)
-    r = 0.5
-    l = 3.0
-    l = cₗ(r,l)
-    a = cₐ(r,l)
-    gᵣ = log(2)/1.15
-    return (2*d.d/(l*gᵣ))^2 *(π*a)
-end
-
-
-function N₀(d::DeathRateTime)
-    r = 0.5
-    l = 3.0
-    l = cₗ(r,l)
-    a = cₐ(r,l)
-    d = log(2)/d.d
-    gᵣ = log(2)/1.15
-    return (2d/l*gᵣ)^2 *(π*a)
-end
-
-
-function N₀(tₐ::ApoptosisTime)
-    r = 0.5
-    l = 3.0
-    l = cₗ(r,l)
-    a = cₐ(r,l)
-    d = log(2)/tₐ.tₐ
-    gᵣ = log(2)/1.15
-    return (2d/l*gᵣ)^2 *(pi*a)
-end
-
-function N₀(R₀::Radius)
-    r = 0.5
-    l = 3.0
-    a = cₐ(r,l)
-    
-    return π*R₀.r^2/a
-end
-
-function R₀(N₀)
-    r = 0.5
-    l = 3.0
-    a = cₐ(r,l)
-    return sqrt(N₀*a/π)
-end
-
-function dᵣ(N₀)
-    r = 0.5
-    l = 3.0
-    l = cₗ(r,l)
-    a = cₐ(r,l)
-    gᵣ = log(2)/1.15
-    return ((l*gᵣ)*√(N₀/(π*a)))/2
-end
-
-function dᵣ(t::TimeForDeathRate)
-    return DeathRate(log(2)/t.t)
-end
-    
-
-function β(d::DeathRate)
-    cᵣ = 0.5
-    cylₗ = 3.0
-    l = cₗ(cᵣ,cylₗ)
-    a = cₐ(cᵣ,cylₗ)
-    return (2*d.d*√(π*a))/(l)
-end
-
-
-
+"""
+    Compute the time to extinction.
+"""
 function compute_time_extinction(N₀,β,r)
     twodivr = 2/r
     βdivr = β/r
@@ -361,6 +403,10 @@ function compute_time_extinction(N₀,β,r)
 end
 
 
+"""
+    Take data path with growth rates of both targets and attackers
+    and returns the time series and older model of the competition.
+"""
 function simulation_population_TS(::MachineState,path,growth_rate₀,growth_rate₁)
     Nₜ = @chain path begin
         readdlm(_) 
@@ -370,4 +416,79 @@ function simulation_population_TS(::MachineState,path,growth_rate₀,growth_rate
                 growth_rate₀,growth_rate₁)
     end
     return Nₜ
+end
+
+"""
+    Compute the time series of the targets population per 
+    simulation and analytical solution.
+"""
+function simulation_analytical(
+    path,
+    growth_rate₀,
+    growth_rate₁,    
+    radius,
+    time_for_death)
+
+    Ñ₀ = N₀(Radius(radius))
+
+    β̃  = β(dᵣ(TimeForDeathRate(time_for_death)))
+    time_death = compute_time_extinction.(Ñ₀,β̃ ,growth_rate₀)
+
+
+    Ñₜ = simulation_population_TS(
+        MachineState(),path,growth_rate₀,growth_rate₁)
+    Nₛₒₗ = compute_pop_ode_sol(Ñₜ.time,Ñₜ.target[1],r,β̃ ) 
+    Nₛₒₗ[findall(x -> x > time_death.re,Ñₜ.time)] .= 0
+
+    return (N_sim = Ñₜ,N_ana = Nₛₒₗ)
+end
+
+
+
+
+
+"""
+    NOT GENERALISED
+    Specific to path such that folder names separated by `/` or `\\` are removed by four times,then the remaining path removed all folder names until a single folder name remains. Here the radius to detect a neigbour is displayed.
+"""
+function get_neighbour_radius(data_path)
+    neighbour_radius_str = @chain data_path begin
+        dirname(_)
+        dirname(_)
+        dirname(_)
+        dirname(_)
+        basename(_)
+    end
+    return neighbour_radius_str
+end
+
+
+"""
+    NOT GENERALISED
+    Specific to path such that folder names separated by `/` or `\\` are removed by three times,then the remaining path removed all folder names until a single folder name remains. Here the radius to determine the size of the targets is displayed.
+"""
+function get_radius(data_path) 
+    radius_str = @chain data_path begin
+        dirname(_)
+        dirname(_)
+        dirname(_)
+        basename(_)
+        replace(_,"Radius_" => "")
+        parse(Float64,_)
+    end
+    return radius_str
+end
+"""
+    NOT GENERALISED
+    Specific to path such that folder names separated by `/` or `\\` are removed by two times,then the remaining path removed all folder names until a single folder name remains. Here the realisation number is displayed.
+"""
+function get_realisation_number(data_path)
+    realisation = @chain data_path begin
+        dirname(_)
+        dirname(_)
+        basename(_)
+        replace(_,"Realisation_" => "")
+        parse(Float64,_)
+    end
+    return realisation
 end
