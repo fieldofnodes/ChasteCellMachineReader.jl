f92819d899afc8146e4c8018de36eb30c1ceab8c
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2023-12-22 23:34:52 +0000

    Added script and data

---
+++ 20221222_add_general_data_to_csv_function.jl
@@ -0,0 +1,9 @@
+##################################################################
+# Filename  : 20221222_add_general_data_to_csv_function.jl
+# Author    : Jonathan Miller
+# Date      : 2023-12-22
+# Aim       : aim_script
+#           : Add functionality to package to read any .dat file
+#           : and turn to CSV
+##################################################################
+
